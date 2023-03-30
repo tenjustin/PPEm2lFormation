e859084916fdd50e7183e62c61c9923e4de616e4
tenjustin <tene.justin@gmail.com>
2023-03-30 02:32:02 +0200

    style

--- include/header.php
+++ include/header.php
@@ -4,10 +4,11 @@
 <head>
 <meta charset="utf-8">
 <link rel="stylesheet" type="text/css" href="style/style.css">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto">
 <title><?php  echo isset($titre)? $titre : 'La Maison des Ligues' ?></title>
 </head>
 <body>
 
 <div id="titre">
-	<h1>Formation Maison des Ligues</h1>
+	<h1 id="title">Formation Maison des Ligues</h1>
 </div>
